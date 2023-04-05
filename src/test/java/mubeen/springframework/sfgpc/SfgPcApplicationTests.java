fc19f4d56de949cf30d5538c76cb593e850c9c0f
Mubeen Sayyed <mubeen@Mubeens-MacBook-Pro.local>
2023-04-06 00:56:27 +0530

    initial commit

---
+++ src/test/java/mubeen/springframework/sfgpc/SfgPcApplicationTests.java
@@ -0,0 +1,13 @@
+package mubeen.springframework.sfgpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
