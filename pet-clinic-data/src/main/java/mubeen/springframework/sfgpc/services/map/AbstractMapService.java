54205cab8dbcf13ca9c21b742b71a820caac457c
Mubeen Sayyed <mubeen@Mubeens-MacBook-Pro.local>
2023-06-20 13:27:48 -0400

    added static resources.Added images and fonts

--- pet-clinic-data/src/main/java/mubeen/springframework/sfgpc/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/mubeen/springframework/sfgpc/services/map/AbstractMapService.java
@@ -1,12 +1,15 @@
 package mubeen.springframework.sfgpc.services.map;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID> {
-	protected Map<ID, T> map = new HashMap<>();
+import mubeen.springframework.sfgpc.model.BaseEntity;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+	protected Map<Long, T> map = new HashMap<>();
 	
 	Set<T> findAll(){
 		return new HashSet<>(map.values());
@@ -16,8 +19,11 @@ public abstract class AbstractMapService<T, ID> {
 		return map.get(id);
 	}
 	
-	T save(ID id, T object) {
-		map.put(id, object);
+	T save(T object) {
+		if(object != null && object.getId() == null) {
+			object.setId(nextId());
+		}
+		map.put(object.getId(), object);
 		return object;
 	}
 	
@@ -27,5 +33,13 @@ public abstract class AbstractMapService<T, ID> {
 	
 	void delete(T object) {
 		map.entrySet().removeIf(entry->entry.getValue().equals(object));
+	}
+	
+	Long nextId() {
+		try {
+			return Collections.max(map.keySet())+1;
+		}catch(Exception e) {
+			return 1l;
+		}
 	}
 }
--- pet-clinic-web/src/main/java/mubeen/springframework/sfgpc/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/mubeen/springframework/sfgpc/bootstrap/DataLoader.java
@@ -28,27 +28,23 @@ public class DataLoader implements CommandLineRunner{
 		// TODO Auto-generated method stub
 		Owner owner = new Owner();
 		owner.setFirstName("Mahi");
-		owner.setId(1L);
 		owner.setLastName("Kummara");
 		
 		ownerService.save(owner);
 		
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Srikanth");
-		owner2.setId(2L);
 		owner2.setLastName("Kadavakollu");
 		ownerService.save(owner2);
 		
 		Vet vet = new Vet();
 		vet.setFirstName("Kumari");
-		vet.setId(1L);
 		vet.setLastName("Nanda");
 		vetService.save(vet);
 	
 		
 		Vet vet2 = new Vet();
 		vet2.setFirstName("Gasonlina");
-		vet2.setId(2L);
 		vet2.setLastName("Frank");
 		vetService.save(vet2);
 	}
